4a2928fa9d089a0b25d21992901f1612ac91b99c
crumpstrr33 <jscott137@pm.me>
2023-06-07 13:44:48 -0500

    Change name to CoVVVR
    Final time, I promise.

--- src/covvvr/__init__.py
+++ src/covvvr/__init__.py
@@ -4,4 +4,4 @@ from .cvintegrator import CVIntegrator, quick_integrate
 from .functions import make_func
 
 __all__ = ["make_func", "CVIntegrator", "quick_integrate"]
-__version__ = version("congas")
+__version__ = version("covvvr")
