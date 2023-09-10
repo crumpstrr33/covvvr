b9df38bd835e984cc8cee87ca5f98fd31c3bfccc
crumpstrr33 <jscott137@pm.me>
2023-09-09 20:27:10 -0500

    Change quick_integrate name pt. 2

--- src/covvvr/__init__.py
+++ src/covvvr/__init__.py
@@ -1,6 +1,6 @@
 from importlib.metadata import version
 
-from .cvintegrator import CVIntegrator, quick_integrate
+from .cvintegrator import CVIntegrator, classic_integrate
 from .functions import make_func
 
 __all__ = ["make_func", "CVIntegrator", "quick_integrate"]
