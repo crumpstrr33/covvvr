56fc65a32822151244f5b9062803a71aba8aeaf2
crumpstrr33 <jscott137@pm.me>
2023-09-10 19:18:47 -0500

    Forgot this change for classic_integrate

--- src/covvvr/__init__.py
+++ src/covvvr/__init__.py
@@ -3,5 +3,5 @@ from importlib.metadata import version
 from .cvintegrator import CVIntegrator, classic_integrate
 from .functions import make_func
 
-__all__ = ["make_func", "CVIntegrator", "quick_integrate"]
+__all__ = ["make_func", "CVIntegrator", "classic_integrate"]
 __version__ = version("covvvr")
